7fc9dccfc920b3e7ba02ed80f898fa7483b19859
shizeying <w741069229@163.com>
2019-01-16 21:28:07 +0800

    快排

---
+++ src/com/algorithm/recursiveAlgorithm/QuickTest.java
@@ -0,0 +1,10 @@
+package com.algorithm.recursiveAlgorithm;
+
+public class QuickTest {
+	public static void main(String[] args) {
+		QuickSortAlgorithm qsa = new QuickSortAlgorithm();
+		int[] nums = { 10, 20, 15, 0, 6, 7, 2, 1, -5, 55 };
+		qsa.printArr(nums);
+	}
+
+}
